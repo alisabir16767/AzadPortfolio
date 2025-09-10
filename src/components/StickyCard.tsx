466146d4d1717fdccd6c710f9d3d0bd43d35afed
Sabir Ali <alisabir167167@gmail.com>
2025-09-10 18:51:21 +0530

    uyjh

--- src/components/StickyCard.tsx
+++ src/components/StickyCard.tsx
@@ -7,11 +7,11 @@ const StickyCard = () => {
         href="https://www.linkedin.com/in/sabirali123/"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center bg-blue-600 text-white px-3 sm:px-4 py-2 sm:py-3 rounded-full shadow-lg hover:bg-blue-700 transition-all text-sm sm:text-base"
+        className="flex items-center bg-green-500 text-white px-3 sm:px-4 py-2 sm:py-3 rounded-full shadow-lg hover:bg-green-700 transition-all text-sm sm:text-base"
       >
         <User size={20} className="mr-2" />
         <span className="font-medium text-center">
-          Develop By ~ Sabir Ali (click to contact)
+          Develop By ~ Sabir Ali (click)
         </span>
       </a>
     </div>
